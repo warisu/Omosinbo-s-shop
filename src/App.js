adcdd6aa509ca8f59883bdb9ee5458a3cde2acd1
warisu <abdullahiabdulwaris@gmail.comgit config --global user.email abdullahiabdulwaris@gmail.com>
2023-05-24 22:07:51 +0100

    Add nav-bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {Cta, Navbar, Feature } from './components';
+import {Cta, Navbar } from './components';
 import { Features, Footer, Header } from './containers';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <div className='gradient__nh'>
+          <Navbar />
+          <Header />
+        </div>
+        <Features />
+        <Cta />
+        <Footer />
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 import './navbar.css';
 
+const Menu = () => ( 
+<>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#products">Products</a></p>
+    <p><a href="#explore">Explore</a></p>
+    <p><a href="#store">Store Locator</a></p>
+    <p><a href="#contacts">Contact</a></p>
+    <p><a href="#carts">Cart</a></p>
+</>
+    )
+
 const Navbar = () => {
-    < div>
+    // const [toggleMenu, setToggleMenu] = useState(false);
+    <div className='shop__navbar-links'>
+        <div className='shop__navbar-links_logo'>
+            <img src={logo} alt="logo" />
+        </div>
+        <div className='shop__navbar-links_container'>
+            <Menu />
+        </div>
     </ div>
 }
 
